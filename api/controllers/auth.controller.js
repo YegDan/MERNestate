d2000758eb8020e109e8bd33ea42ea17c68fd222
Yeganeh <vnus.daneshparvar@gmail.com>
2024-12-09 00:47:36 -0500

    sign in api created

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs"
 import { errorHandler } from "../utils/error.js"
+import jwt from "jsonwebtoken"
+
 export const signup = async (req, res, next)=>{
     
     const {username, email, password} = req.body
@@ -14,4 +16,24 @@ export const signup = async (req, res, next)=>{
         next(error)
     }
     
+}
+
+export const signin = async (req, res, next)=>{
+    const {email, password} = req.body
+    try{
+        const user = await User.findOne({email})
+        if(!user){
+            return errorHandler(404, "User not found")
+        }
+        if(!bcryptjs.compareSync(password, user.password)){
+            res.status(401).json("Invalid credentials")
+        }
+        const {password: hashedPass, ...rest} = user._doc
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
+        res.cookie("token", token, {httpOnly: true, expires: new Date(Date.now() + 24*60*60*1000)})
+        .status(200)
+        .json(rest)
+    }catch(error){
+        next(error)
+    }
 }
